c1af72657154a6163536cb1b9ace1ac751c88173
Jaiminpro-org <yojaimin5@gmail.com>
2026-01-12 15:30:20 +0530

    HMM

--- script.js
+++ script.js
@@ -2,21 +2,12 @@ const chat = document.getElementById("chat");
 const input = document.getElementById("messageInput");
 const button = document.getElementById("sendBtn");
 
-button.addEventListener("click", sendMessage);
-input.addEventListener("keydown", function (e) {
-  if (e.key === "Enter") {
-    sendMessage();
-  }
-});
-
-
 function sendMessage() {
   const text = input.value.trim();
   if (text === "") return;
 
   const msg = document.createElement("div");
-  msg.className = "message me";
-
+  msg.classList.add("message", "me");
   msg.innerText = text;
 
   chat.appendChild(msg);
@@ -24,24 +15,10 @@ function sendMessage() {
   chat.scrollTop = chat.scrollHeight;
 }
 
-.message {
-  max-width: 70%;
-  padding: 8px;
-  margin-bottom: 6px;
-  border-radius: 8px;
-  word-wrap: break-word;
-}
+button.addEventListener("click", sendMessage);
 
-.me {
-  background: #0084ff;
-  color: white;
-  margin-left: auto;
-  text-align: right;
-}
-
-.other {
-  background: #e4e6eb;
-  color: black;
-  margin-right: auto;
-  text-align: left;
-}
+input.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    sendMessage();
+  }
+});
