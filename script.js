21e4333e44ecb1150c2d5fae869d934a25894480
Jaiminpro-org <yojaimin5@gmail.com>
2026-01-12 15:25:21 +0530

    NEW

--- script.js
+++ script.js
@@ -15,10 +15,33 @@ function sendMessage() {
   if (text === "") return;
 
   const msg = document.createElement("div");
-  msg.className = "message";
+  msg.className = "message me";
+
   msg.innerText = text;
 
   chat.appendChild(msg);
   input.value = "";
   chat.scrollTop = chat.scrollHeight;
+}
+
+.message {
+  max-width: 70%;
+  padding: 8px;
+  margin-bottom: 6px;
+  border-radius: 8px;
+  word-wrap: break-word;
+}
+
+.me {
+  background: #0084ff;
+  color: white;
+  margin-left: auto;
+  text-align: right;
+}
+
+.other {
+  background: #e4e6eb;
+  color: black;
+  margin-right: auto;
+  text-align: left;
 }
